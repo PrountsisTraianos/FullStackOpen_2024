8b923c489e97df5b7ce9d5912355ad42bd159789
PrountsisTraianos <traianpron@gmail.com>
2024-06-04 22:40:23 +0300

    Answer to Exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,29 +1,5 @@
 /* eslint-disable react/prop-types */
-
-const Header = ({ course }) => <h1>{course.name}</h1>
-
-const Total = ({ course }) => 
-{
-  let total = course.parts.reduce((summedPart, currentPart ) => {return summedPart + currentPart.exercises},0) 
-  // console.log("sum", total);
-
-  return(
-    <p>Number of exercises {total}</p>
-  )
-}
-
-const Part = ({ course }) => <>{ course.parts.map( part =>
-    <li key = { part.id } > { part.name } { part.exercises } </li>)} 
-  </>
-
-const Content = ({ course }) => <ul> <Part course = { course } /> </ul>
-
-const Course = ({ course }) =>
-  <> 
-  <Header course= { course } />
-  <Content course ={ course } />
-  <Total course = { course } />
-  </>
+import Course from './components/Course'
 
 
 const App = () => {
@@ -76,4 +52,5 @@ const App = () => {
     return mappedCourses
 
 }
+
 export default App
