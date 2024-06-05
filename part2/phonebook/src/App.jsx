94b080706418f346db3c71dc2c0c62c235c595e7
PrountsisTraianos <traianpron@gmail.com>
2024-06-05 19:06:45 +0300

    Answer to Exercise 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
+
+const Number = ({person}) => <p> {person.name} </p>
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  } 
+
+  const handleNameChange = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value);
+  }
+  
+  const mapPerson = persons.map((person) => <Number key={person.name} person ={person}/>)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value ={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>{mapPerson}</div>
+    </div>
+  )
+}
+
+export default App
